5794851f7bfb09bf8c53e54ab017723c7c3711a3
hirotomo <hero.rugby1015@gmail.com>
2021-01-12 23:18:10 +0900

    wheel.js

---
+++ wheel.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+
+    var wheel = new wheelnav("wheelDiv");
+
+    //This is the place for code snippets from the documentation -> http://wheelnavjs.softwaretailoring.net/documentation.html
+
+    wheel.createWheel(["Works", "Skills", "Career"]);
+    wheel.navigateWheel(2); //default num
+    wheel.titleAttr = { fill: "#FFF" };
+    wheel.titleSelectedAttr = { fill: "#111" };
+    wheel.titleHoverAttr = { fill: "#111" };
+    wheel.refreshWheel();
+
+    //Insert generated JavaScript code from here -> http://pmg.softwaretailoring.net
+
+};
