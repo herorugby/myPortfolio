9b29315a90fae49910465ddb98034ec3d875a5de
hirotomo <hero.rugby1015@gmail.com>
2021-01-13 15:13:52 +0900

    renew files

--- wheel.js
+++ wheel.js
@@ -1,6 +1,7 @@
+var wheel = new wheelnav("wheelDiv");
 window.onload = function () {
 
-    var wheel = new wheelnav("wheelDiv");
+    // var wheel = new wheelnav("wheelDiv");
 
     //This is the place for code snippets from the documentation -> http://wheelnavjs.softwaretailoring.net/documentation.html
 
@@ -9,8 +10,9 @@ window.onload = function () {
     wheel.titleAttr = { fill: "#FFF" };
     wheel.titleSelectedAttr = { fill: "#111" };
     wheel.titleHoverAttr = { fill: "#111" };
-    wheel.refreshWheel();
 
     //Insert generated JavaScript code from here -> http://pmg.softwaretailoring.net
-
-};
+    wheel.navItems[0].navigateFunction = function () {
+        alert("Clicked homepage.");
+      }
+}
