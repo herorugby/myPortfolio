57a90c63ba292d1fc6efc4c5c866497e36759502
hirotomo <hero.rugby1015@gmail.com>
2021-01-15 13:27:33 +0900

    new

--- js/index.js
+++ js/index.js
@@ -1,35 +1,38 @@
 var a = document.getElementById("about-me");
-var b = document.getElementById("works");
-var c = document.getElementById("skills");
-var d = document.getElementById("future");
+// var b = document.getElementById("works");
+// var c = document.getElementById("skills");
+// var d = document.getElementById("future");
 
-// a.style.display = "none";
-
-
+a.style.display = "none";
 
 
-
-var wheel = new wheelnav("wheelDiv");
 window.onload = function () {
 
-    // var wheel = new wheelnav("wheelDiv");
+    var wheel = new wheelnav("wheelDiv");
 
     //This is the place for code snippets from the documentation -> http://wheelnavjs.softwaretailoring.net/documentation.html
 
     wheel.createWheel(["About ME", "Works", "Skills", "Future"]);
     wheel.navigateWheel(0); //default num
+    wheel.navAngle = 45;
     wheel.titleAttr = { fill: "#FFF" };
     wheel.titleSelectedAttr = { fill: "#111" };
     wheel.titleHoverAttr = { fill: "#111" };
+    wheel.refreshWheel();
+    wheel.titleRotateAngle = 50;
 
     //Insert generated JavaScript code from here -> http://pmg.softwaretailoring.net
-    wheel.navItems[0].navigateFunction = function () {
-      addEventListener("click", addVal);
-      }
 
-      function addVal() {
-        a.style.display = "block";
-      }
+    wheel.navItems[0].navigateFunction = function () {
+      a.style.display = "block";
+    };
+    // wheel.navItems[0].navigateFunction = function () {
+    //   addEventListener("click", addVal);
+    //   }
+
+    //   function addVal() {
+    //     a.style.display = "block";
+    //   }
 }
 
 
