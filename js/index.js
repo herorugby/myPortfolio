86c5022d5a040e5645e0eec103dc82463d2f2104
hirotomo <hero.rugby1015@gmail.com>
2021-01-16 00:56:59 +0900

    fixed

--- js/index.js
+++ js/index.js
@@ -3,50 +3,42 @@ var a = document.getElementById("about-me");
 // var c = document.getElementById("skills");
 // var d = document.getElementById("future");
 
-a.style.display = "none";
+// a.style.display = "none";
 
 
-window.onload = function () {
-
-    var wheel = new wheelnav("wheelDiv");
-
-    //This is the place for code snippets from the documentation -> http://wheelnavjs.softwaretailoring.net/documentation.html
-
-    wheel.createWheel(["About ME", "Works", "Skills", "Future"]);
-    wheel.navigateWheel(0); //default num
-    wheel.navAngle = 45;
-    wheel.titleAttr = { fill: "#FFF" };
-    wheel.titleSelectedAttr = { fill: "#111" };
-    wheel.titleHoverAttr = { fill: "#111" };
-    wheel.refreshWheel();
-    wheel.titleRotateAngle = 50;
-
-    //Insert generated JavaScript code from here -> http://pmg.softwaretailoring.net
-
-    wheel.navItems[0].navigateFunction = function () {
-      a.style.display = "block";
-    };
-    // wheel.navItems[0].navigateFunction = function () {
-    //   addEventListener("click", addVal);
-    //   }
-
-    //   function addVal() {
-    //     a.style.display = "block";
-    //   }
-}
+var wheel = new wheelnav('wheelDiv');
+      wheel.createWheel(["About ME", "Works", "Skills", "Future"]);
+      wheel.navigateWheel(0); //default num
 
 
+// window.onload = function () {
+
+//     // var wheel = new wheelnav('wheelDiv');
+
+//     //This is the place for code snippets from the documentation -> http://wheelnavjs.softwaretailoring.net/documentation.html
+
+//     // wheel.createWheel(["About ME", "Works", "Skills", "Future"]);
+//     // wheel.navigateWheel(0); //default num
+
+//     //Insert generated JavaScript code from here -> http://pmg.softwaretailoring.net
+
+//     wheel.navItems[0].navigateFunction = function () {
+//       // a.style.display = "block";
+//     };
+//     // wheel.navItems[0].navigateFunction = function () {
+//     //   addEventListener("click", addVal);
+//     //   }
+
+//     //   function addVal() {
+//     //     a.style.display = "block";
+//     //   }
+// }
 
 
 // copy right get full year
-// get Date preparation
-let dayOption = new Date();
-let myYear = dayOption.getFullYear();
-
 $(function() {
-  function copyRightYear() {
-    copyRight = $("#copyRight");
-    copyRight.text("2020-" + myYear);
-  }
-  copyRightYear();
+  let dayOption = new Date();
+  let myYear = dayOption.getFullYear();
+  let copyRight = $("#copyRight");
+      copyRight.text("2020-" + myYear);
 });
